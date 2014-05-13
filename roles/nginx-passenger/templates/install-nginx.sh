33b3ee510081ee6954d698b4eb7c5c7b57ba42c7
Jason Frame <jason@onehackoranother.com>
2014-05-13 17:25:14 +0100

    use sudo to install passenger so that ruby is in path :/

--- roles/nginx-passenger/templates/install-nginx.sh
+++ roles/nginx-passenger/templates/install-nginx.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+export PATH={{ ruby_path }}:$PATH
+
 cd {{ source_directory }}
 tar xvfz nginx-{{ nginx_version }}.tar.gz
-sudo {{ ruby_location }}/bin/passenger-install-nginx-module \
+{{ ruby_location }}/bin/passenger-install-nginx-module \
 	--auto \
 	--prefix="{{ nginx_location }}" \
 	--nginx-source-dir="{{ source_directory }}/nginx-{{ nginx_version }}" \
 	--extra-configure-flags="{{ nginx_extra_configure_flags }}"
-
